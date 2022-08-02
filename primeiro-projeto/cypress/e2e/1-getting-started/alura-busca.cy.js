a3607260ed8405c72b259f86fc61d92af86862cb
Matheus Torres <matheus@coliseum.gg>
2022-08-02 18:08:13 -0300

    chore: initial commit + first project

---
+++ primeiro-projeto/cypress/e2e/1-getting-started/alura-busca.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe("alura busca cursos", () => {
+  beforeEach(() => {
+    cy.visit("https://www.alura.com.br/");
+  });
+  it("buscar curso de java", () => {
+    cy.get("#header-barraBusca-form-campoBusca").type("java");
+    cy.get(".header-barraBusca-form-submit").click();
+    cy.get("h4.busca-resultado-nome").should("contain", "Java");
+  });
+});
